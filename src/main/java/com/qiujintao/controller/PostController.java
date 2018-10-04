6ad4d17a2520e94cc52672b5257ace586b49a216
alanHarper123 <1531508001@qq.com>
2018-10-04 19:05:14 +0800

    minor errors fixed

--- src/main/java/com/qiujintao/controller/PostController.java
+++ src/main/java/com/qiujintao/controller/PostController.java
@@ -1,7 +1,5 @@
 package com.qiujintao.controller;
 
-<<<<<<< HEAD
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -97,54 +95,6 @@ public class PostController {
 		String result = "deleted update no."+payload.get("update_id");
 		System.out.println("deleted update no."+payload.get("update_id"));
 		return "{\"result\": \""+result+"\"}";
-=======
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-
-import com.qiujintao.model.Article;
-import com.qiujintao.model.User;
-import com.qiujintao.service.ArticleService;
-import com.qiujintao.service.MyUserDetails;
-
-@RestController
-public class PostController {
-	
-	@Autowired
-	private ArticleService articleService;
-	@PostMapping("/new-post/api/article/save")
-	public String postArticle(@Valid @RequestBody Article article) {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		
-		if(!(authentication instanceof AnonymousAuthenticationToken)) {
-			
-			try {
-				MyUserDetails myUserDetails = (MyUserDetails)authentication.getPrincipal();
-				article.setUser_id(myUserDetails.getUser().getId());
-				if(article.getId() != 0) {
-					articleService.saveArticle(article);
-				}else {
-					articleService.updateArticle(article);
-				}
-				return "Post Successfully";
-			} catch (Exception e) {
-				return "error: unable to save article "+e.getMessage();
-			}
-		}else {
-			return "please login first";
-		}
->>>>>>> refs/remotes/origin/master
 	}
 	@GetMapping(value="/api/article/delete", params="id")
 	public String deleteArticle(@RequestParam int id) {
